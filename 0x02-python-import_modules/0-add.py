08616cb32bfadf266ff39635fbaaa5d8e9989ad8
Yashar Mateen <yasheymateen@gmail.com>
2019-09-14 04:36:38 +0000

    modified 0-add file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import add_0
+from add_0 import add
+
+def main():
     a = 1
     b = 2
-    from add_0 import add
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+if __name__ == '__main__':
+    main()
