1bbe4b898e6e471f9910343b15e5533d9f90c034
Yashar Mateen <yasheymateen@gmail.com>
2019-09-20 05:02:38 +0000

    modified 12 roman file for pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     value = 0
     num = 0
-    numeral = roman_string
+    numer = roman_string
     if type(roman_string) is not str:
         return 0
     if roman_string is None:
         return 0
-    for num in range (num, len(numeral)):
-        if num < len(numeral) - 1 and my_dict[numeral[num]] < my_dict[numeral[num + 1]]:
-            value -= my_dict[numeral[num]]
+    for num in range (num, len(numer)):
+        if num < len(numer) - 1 and dict[numer[num]] < dict[numer[num + 1]]:
+            value -= dict[numer[num]]
         else:
-            value += my_dict[numeral[num]]
+            value += dict[numer[num]]
     return value
