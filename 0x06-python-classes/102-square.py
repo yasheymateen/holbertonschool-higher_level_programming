c5910e0679d7f315ab317e568fba334a967a800f
Yashar Mateen <yasheymateen@gmail.com>
2019-10-02 04:46:14 +0000

    added 101 and 102 task files

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""class Square"""
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+        """initialize with attributes"""
+        self.size = size
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets size with error handling"""
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """to find area of square"""
+        return self.__size * self.__size
+
+    def __lt__(self, other):
+        """less than"""
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        """less than or equals"""
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        """equal to"""
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        """not equal to"""
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        """greater than"""
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        """greater than"""
+        return self.area() >= other.area()
