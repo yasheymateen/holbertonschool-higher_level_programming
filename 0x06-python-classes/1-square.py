f21aa45bbbcd7b485d8f57412d6d2c368b174839
Yashar Mateen <yasheymateen@gmail.com>
2019-10-02 05:01:56 +0000

    modified for pep8

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,6 +2,6 @@
 """square Class:"""
 class Square:
     """ creates a square size attribute"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """initialize variable"""
-        self.__size__ = size
+        self.__size = size
