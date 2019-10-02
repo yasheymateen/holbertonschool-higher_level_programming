9218dd8d1d156e0a0e3ef2af3f0e3a0afba197fb
Yashar Mateen <yasheymateen@gmail.com>
2019-10-02 05:06:21 +0000

    modified for pep8

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ square Class"""
+
+
 class Square:
     """class variable size"""
+
     def __init__(self, size=0):
         """initialize size"""
-        if type(size) is not int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError('size must be >= 0')
