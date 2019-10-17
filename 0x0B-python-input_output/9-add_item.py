a74dbed6e48050baf6471efa3f83007cf95510ed
Yashar Mateen <yasheymateen@gmail.com>
2019-10-17 07:39:00 +0000

    modified 9 add item file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,24 +2,12 @@
 """ adds all arguments to python list and then saves them """
 
 import sys
-save_to_json_file = __import__('7-save_to_json_file).save_to_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    count = 0
-    try:
-        data = load_from_json_file("add_item.json")
-    except:
-        my_list = []
-        f = open("add_item.json", 'w+')
-        save_to_json_file(my_list, "add_item.json")
-        f.close()
-
-    data = load_from_json_file("add_item.json")
-    for arg in sys.argv:
-        if count == 0:
-            pass
-            count += 1
-        else:
-            data.append(arg)
-    save_to_json_file(data, "add_item.json")
+try:
+    my_list = load_from_json_file('add_item.json')
+    my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, 'add_item.json')
+except FileNotFoundError:
+    save_to_json_file(sys.argv[1:], 'add_item.json')
