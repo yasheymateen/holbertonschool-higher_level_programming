613a9cc9d9469635de3eafc652c7bd0a78a76133
Yashar Mateen <yasheymateen@gmail.com>
2019-10-17 07:25:53 +0000

    modified 7 file

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -2,10 +2,10 @@
 """ writes object to text file """
 
 
-import json
-
-
 def save_to_json_file(my_obj, filename):
     """ save to json """
-    with open(filename, mode'w') as f:
-        f.write(json.dumps(my_obj)
+    if type(filename) != str:
+        raise TypeError("filename must be a string")
+    import json
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
