954bbf3393ccab90972c86d3ee25e3d251a2bf54
Yashar Mateen <yasheymateen@gmail.com>
2019-12-16 01:54:13 +0000

    added 5th task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that lists all cities from the database"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username, password, db_name = sys.argv[1:]
+    cmd = """SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states ON state_id = states.id ORDER BY cities.id"""
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+
+    cur = db.cursor()
+    cur.execute(cmd)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
