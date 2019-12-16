154014880a0cc5f1477f7ba8565f9c01d8431829
Yashar Mateen <yasheymateen@gmail.com>
2019-12-16 02:13:28 +0000

    modified model state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,6 @@ Base = declarative_base()
 class State(Base):
     """ State class that inherits from Base class"""
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True,nullable=False,
-                primary_keys=True)
+    id = Column(Integer, autoincrement=True, nullable=False,
+                primary_key=True)
     name = Column(String(128), nullable=False)
