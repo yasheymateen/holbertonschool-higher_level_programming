e7c664f4e400315c9ce8e88b41fe87a84d7a1dde
yasheymateen <yasheymateen@gmail.com>
2019-12-19 05:34:03 +0000

    modified model state for pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """ State class that inherits from Base class"""
     __tablename__ = 'states'
