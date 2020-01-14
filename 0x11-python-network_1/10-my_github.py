7c7ed1ea6b8290836e4c3f0e0654eb7a10b079f2
yasheymateen <yasheymateen@gmail.com>
2020-01-14 03:28:03 +0000

    added 10 my github mwahaha

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Takes your Github credentials  and uses the Github API to display id """
+import requests
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+
+    r = requests.get(url, auth=(user, password))
+    json_repr = r.json()
+    print(json_repr.get('id', 'None'))
