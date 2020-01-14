d9841071186d4c2bba22f0522afb6ba13e47053e
yasheymateen <yasheymateen@gmail.com>
2020-01-14 03:03:40 +0000

    added 8 task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends POST  request to http://0.0.0.0:5000/search_user 
+with letter as a parameter
+"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'http://831dbdfb081b.41.hbtn-cod.io:5000/search_user'
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    r = requests.post(url, data={'q': letter})
+    try:
+        instance = r.json()
+        if len(instance) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(instance.get('id'), instance.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
