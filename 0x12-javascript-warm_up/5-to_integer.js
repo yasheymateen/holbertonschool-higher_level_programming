e48bac8710453ec4ad91c56095bfc72d047d8606
yasheymateen <yasheymateen@gmail.com>
2020-02-04 23:38:11 +0000

    modified to include the script grammar of ..My number..

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) === false) {
-  console.log(parseInt(process.argv[2]));
-} else {
+if (isNaN(process.argv[2]) === true) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
