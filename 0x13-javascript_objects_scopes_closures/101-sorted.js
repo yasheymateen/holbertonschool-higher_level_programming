a4253c7c38bd14ac96f80a117ab4fe7ca9a18b1b
yasheymateen <yasheymateen@gmail.com>
2020-02-11 03:44:55 +0000

    modified semistandard

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 const occurences = {};
-for (let key in dict) {
+for (const key in dict) {
   if (dict[key] in occurences) {
     occurences[dict[key]].push(key);
   } else {
