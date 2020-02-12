945ef2ce2ac40991aef74c8f0a7001fce72ced48
Yashar Mateen <yasheymateen@gmail.com>
2020-02-12 04:44:13 +0000

    modified 2 status code file

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ request(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ', response.statusCode);
+    console.log('code: ' + response.statusCode);
   }
 });
