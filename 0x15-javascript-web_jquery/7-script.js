ea6e4d29b55a34ea350086a89d72f924eed4f106
Yashar Mateen <yasheymateen@gmail.com>
2020-02-13 22:49:30 -0800

    added next 4 files after testing

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(function () {
+    const url = 'https://swapi.co/api/people/5/?format=json';
+    $.get(url, function (data, textStatus) {
+      $('div#character').text(data.name);
+    });
+  });
+  
---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+$(function () {
+    const lang = $('html').attr('lang');
+    const url = 'https://fourtonfish.com/hellosalut/?lang=' + lang;
+    $.get(url, function (data, textStatus) {
+      $('div#hello').text(data.hello);
+    });
+  });
